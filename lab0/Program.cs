e860c03a021b9158ce40447ad197a97dae41fedc
JEKA <JEKA@DESKTOP-H06U4VA>
2023-09-12 14:38:26 +0300

    Добавьте файлы проекта.

---
+++ lab0/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab0
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Вычисления суммы двух целых чисел
+        /// </summary>
+        /// <param name="first"> Первое целое число</param>
+        /// <param name="second">Второе целое число</param>
+        /// <returns> Сумма двух целых чисел</returns>
+        private static int SumofTwoIntegers (int first,int second) {
+            return  first + second;
+        }
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int first = rnd.Next(100);
+            int second = rnd.Next(100);
+            Console.WriteLine($" \n Первое число:{first} ; Второе число:{second} ; \n \n Cумма целых чисел: {SumofTwoIntegers(first, second)}");
+            Console.ReadLine();
+        }
+    }
+}
